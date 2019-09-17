e127a9a130204cd0c038f46ac0fccb967b26ad6b
Gabrielle <gab.berteloot@gmail.com>
2019-09-17 18:00:04 +0200

    Fonction Loader + Dictionnaire

--- src/fr/wcs/atg/dicoutils/DicoLoader.java
+++ src/fr/wcs/atg/dicoutils/DicoLoader.java
@@ -1,5 +1,24 @@
 package fr.wcs.atg.dicoutils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DicoLoader {
+
+	public static void Loader(String[] args) {
+
+		final String FILENAME = "text.txt";
+
+		try (BufferedReader bufferedreader = new BufferedReader(new FileReader(FILENAME))) {
+			String strCurrentLine;
+			while ((strCurrentLine = bufferedreader.readLine()) != null) {
+				System.out.println(strCurrentLine);
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+
+	}
 
 }
