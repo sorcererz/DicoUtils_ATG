e754e5a23a0cc4d8685fe6058bfad6924ea7f7da
Thomas Buvat <buvat.thomas.pro@gmail.com>
2019-09-18 17:53:19 +0200

    starts of class DicoSearch, byEquals method done

--- src/fr/wcs/atg/dicoutils/DicoUtils.java
+++ src/fr/wcs/atg/dicoutils/DicoUtils.java
@@ -1,17 +1,19 @@
 package fr.wcs.atg.dicoutils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DicoUtils {
 
 	public static void main(String[] args) {
-		 DicoIHM ihm = new DicoIHM();
-		 DicoLoader loader = new DicoLoader();
-		 String[] arrayDico = loader.load();
-		 ihm.display("Enter your name :");
-		 String name = ihm.read(); //new instance of a value typed into the terminal
-		 ihm.display(name);
-		 for (int i = 0; i < arrayDico.length; i++) {
-			ihm.display(arrayDico[i]);
-		}
+		DicoIHM ihm = new DicoIHM();
+		DicoLoader loader = new DicoLoader();
+		DicoSearch search = new DicoSearch();
+		String[] arrayDico = loader.load();
+		ihm.display("Enter your name :");
+		String name = ihm.read(); // new instance of a value typed into the terminal
+		ihm.display(name);
+		String zyth = "(.*)yth";
+		ihm.display(search.byRegex(arrayDico, zyth));
 	}
-
 }
