b79295332f110c6a39fd52086b74946b615c0060
Thomas Buvat <buvat.thomas.pro@gmail.com>
2019-09-19 11:59:23 +0200

    translated in french

--- src/fr/wcs/atg/dicoutils/DicoUtils.java
+++ src/fr/wcs/atg/dicoutils/DicoUtils.java
@@ -21,31 +21,31 @@ public class DicoUtils {
 				ihm.display("Sortie");
 				break;
 			case 1:
-				ihm.display("Enter your word :");
+				ihm.display("Entrez un mot :");
 				String type1 = ihm.read(); // new instance of a value typed into the terminal
 				ihm.display(type1);
 				ihm.display(search.byEquals(arrayDico, type1));
 				break;
 			case 2:
-				ihm.display("Enter your word :");
+				ihm.display("Entrez un mot :");
 				String type2 = ihm.read(); // new instance of a value typed into the terminal
 				ihm.display(type2);
 				ihm.display(search.byContains(arrayDico, type2));
 				break;
 			case 3:
-				ihm.display("Enter your word :");
+				ihm.display("Entrez un mot :");
 				String type3 = ihm.read(); // new instance of a value typed into the terminal
 				ihm.display(type3);
 				ihm.display(search.byStartsWith(arrayDico, type3));
 				break;
 			case 4:
-				ihm.display("Enter your word :");
+				ihm.display("Entrez un mot :");
 				String type4 = ihm.read(); // new instance of a value typed into the terminal
 				ihm.display(type4);
 				ihm.display(search.byEndsWith(arrayDico, type4));
 				break;
 			case 5:
-				ihm.display("Enter your word :");
+				ihm.display("Entrez un mot :");
 				String type5 = ihm.read(); // new instance of a value typed into the terminal
 				ihm.display(type5);
 				ihm.display(search.byRegex(arrayDico, type5));
