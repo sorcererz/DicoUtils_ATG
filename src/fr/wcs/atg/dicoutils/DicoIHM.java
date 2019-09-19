82d016c660be7ba84e97f33e9e2995aa2b95fbd3
MKHENINI <adel.mkhenini@gmail.com>
2019-09-19 11:49:36 +0200

    Complete

--- src/fr/wcs/atg/dicoutils/DicoIHM.java
+++ src/fr/wcs/atg/dicoutils/DicoIHM.java
@@ -12,6 +12,15 @@ public class DicoIHM {
 		display("Welcome to our dictionary!");
 	}
 	
+	public void displayChoice() {
+        display("1 = Rechercher la présence d'un mot exacte ");
+        display("2 = Rechercher tous les mots qui commencent par une valeur ");
+        display("3 = Rechercher tous les mots qui terminent par une valeur ");
+        display("4 = Rechercher tous les mots qui contiennent une valeur ");
+        display("5 = Rechercher des mots par une expression régulière ");
+        display("0 = Pour quitter ");
+    }
+	
 	public String read() {
 		Scanner scan = new Scanner(System.in); //initialize the Scanner object and read the value of the terminal
 		return scan.next(); //return the next string the user types in the terminal
--- src/fr/wcs/atg/dicoutils/DicoUtils.java
+++ src/fr/wcs/atg/dicoutils/DicoUtils.java
@@ -7,12 +7,52 @@ public class DicoUtils {
 		DicoLoader loader = new DicoLoader();
 		DicoSearch search = new DicoSearch();
 		String[] arrayDico = loader.load();
-		ihm.display("Enter your name :");
-		String name = ihm.read(); // new instance of a value typed into the terminal
-		ihm.display(name);
-		String zyth = "z";
-		ihm.display(search.byEquals(arrayDico, zyth));
-		String answer = search.byStartsWith(arrayDico, zyth);
-		ihm.display(answer);
+		ihm.displayWelcome();
+		String strChoice;
+		int choice;
+
+		do {
+			ihm.displayChoice();
+			strChoice = ihm.read(); // new instance of a value typed into the terminal
+			choice = Integer.parseInt(strChoice);
+
+			switch (choice) {
+			case 1:
+				ihm.display("Enter your word :");
+				String type1 = ihm.read(); // new instance of a value typed into the terminal
+				ihm.display(type1);
+				ihm.display(search.byEquals(arrayDico, type1));
+				break;
+			case 2:
+				ihm.display("Enter your word :");
+				String type2 = ihm.read(); // new instance of a value typed into the terminal
+				ihm.display(type2);
+				ihm.display(search.byContains(arrayDico, type2));
+				break;
+			case 3:
+				ihm.display("Enter your word :");
+				String type3 = ihm.read(); // new instance of a value typed into the terminal
+				ihm.display(type3);
+				ihm.display(search.byStartsWith(arrayDico, type3));
+				break;
+			case 4:
+				ihm.display("Enter your word :");
+				String type4 = ihm.read(); // new instance of a value typed into the terminal
+				ihm.display(type4);
+				ihm.display(search.byEndsWith(arrayDico, type4));
+				break;
+			case 5:
+				ihm.display("Enter your word :");
+				String type5 = ihm.read(); // new instance of a value typed into the terminal
+				ihm.display(type5);
+				ihm.display(search.byRegex(arrayDico, type5));
+				break;
+			default:
+				ihm.display("Sortie");
+				break;
+			}
+
+		} while (choice != 0);
 	}
 }
+
